6ffb39988635720645d2dec576c1c71809181c33
Santtu Lintervo <santervo@gmail.com>
2012-11-06 23:05:56 +0200

    Color selector

--- js/main.js
+++ js/main.js
@@ -14,6 +14,10 @@ $(function() {
     var canvas = $canvas[0];
     var ctx = canvas.getContext("2d");
 
+    this.setColor = function(color) {
+      ctx.fillStyle = color;
+    };
+
     this.stroke = function(e) {
       ctx.beginPath();
       ctx.arc(e.pageX,e.pageY,10,0,Math.PI*2);
@@ -46,10 +50,26 @@ $(function() {
     };
   };
 
+  var ColorSelector = function(selector, canvas) {
+    var colorSelector = $(selector);
+
+    this.init = function() {
+      colorSelector.find(".button").click(function() {
+        var self = $(this);
+        colorSelector.children().removeClass("selected");
+        self.addClass("selected");
+        canvas.setColor(self.css("background-color"));
+      });
+    };
+  };
+
   var area = new DrawingArea("#drawing-area");
   var canvas = new Canvas("#drawing-area canvas");
+  var colorSelector = new ColorSelector("#color-selector", canvas);
 
   area.resizeCanvas();
   canvas.init();
+  colorSelector.init();
+
   $(window).resize(area.resizeCanvas);
 });
